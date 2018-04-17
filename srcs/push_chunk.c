c49ada6139dff2d2038741a007aabc6505c1d1af
Djelali DINAUT <ddinaut@e1r13p11.42.fr>
2018-04-17 14:29:09 +0200

    first implementation thread safety

--- srcs/push_chunk.c
+++ srcs/push_chunk.c
@@ -6,7 +6,7 @@
 /*   By: ddinaut <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 14:42:26 by ddinaut           #+#    #+#             */
-/*   Updated: 2018/04/17 12:44:03 by ddinaut          ###   ########.fr       */
+/*   Updated: 2018/04/17 14:23:23 by ddinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,9 @@ static void	*push_large(t_area **area, size_t size)
 
 	if ((*area) == NULL)
 	{
+		pthread_mutex_lock(&g_thread);
 		(*area) = create_large_area(size);
+		pthread_mutex_unlock(&g_thread);
 		return ((unsigned char*)*area + AREA_SIZE);
 	}
 	else
@@ -55,7 +57,9 @@ static void	*push_large(t_area **area, size_t size)
 		tmp = (*area);
 		while (tmp->next != NULL)
 			tmp = tmp->next;
+		pthread_mutex_lock(&g_thread);
 		tmp->next = create_large_area(size);
+		pthread_mutex_unlock(&g_thread);
 	}
 	return ((unsigned char*)tmp->next + AREA_SIZE);
 }
@@ -70,18 +74,20 @@ void	*push_chunk_to_area(size_t size)
 	if (size <= TINY_SIZE)
 	{
 		if ((ret = search_free_chunk(g_page.small, size)) != NULL)
-		{
 			return (ret);
-		}
+		pthread_mutex_lock(&g_thread);
 		area = search_small_area(size, &g_page.small);
 		ret = push_chunk(area, size);
+		pthread_mutex_unlock(&g_thread);
 	}
 	else if (size <= MEDIUM_SIZE)
 	{
 		if ((ret = search_free_chunk(g_page.medium, size)) != NULL)
 			return (ret);
+		pthread_mutex_lock(&g_thread);
 		area = search_medium_area(size, &g_page.medium);
 		ret = push_chunk(area, size);
+		pthread_mutex_unlock(&g_thread);
 	}
 	else
 		ret = push_large(&g_page.large, size);
