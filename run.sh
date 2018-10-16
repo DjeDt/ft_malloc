953d4d42724a70dab5f525db7d063e0711a14eca
DjeDt <djelali.dinaut@gmail.com>
2018-10-17 00:10:47 +0200

    update run script to use it on linux distro

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-export DYLD_LIBRARY_PATH="."
-export DYLD_INSERT_LIBRARIES="libft_malloc.so"
-export DYLD_FORCE_FLAT_NAMESPACE=1
-$@
+OS=$(uname -s)
+if [ "${OS}" == "Linux" ] ; then
+    export LD_PRELOAD=/home/dje/Dev/ft_malloc/libft_malloc_x86_64_Linux.so
+    $@
+else
+    export DYLD_LIBRARY_PATH="."
+    export DYLD_INSERT_LIBRARIES="libft_malloc.so"
+    export DYLD_FORCE_FLAT_NAMESPACE=1
+    $@
+fi
